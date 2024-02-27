a579a3fe3558f2c5019f41d0df29b271e9fc35fd
Angel Chavira <achavira@umich.edu>
2024-02-26 20:05:28 -0500

    Created class Analysis to create histograms
    The Analysis class creates histograms of the dimer searches from OPLD
    
    Created a python script to use Analysis class and make plots

---
+++ analysis.py
@@ -0,0 +1,58 @@
+import os
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import math
+
+BOLTZMANN_CONSTANT = 8.617333262145e-5
+
+class Analysis():
+
+  def __init__(self, dataFile, temperature=300.0):
+    self.dataFile = os.path.abspath(dataFile)
+    self.fileDir = os.path.dirname(self.dataFile)
+    self.temperature = temperature
+    # self.activationEnergies = np.full(MAX_TRANSITION_STATES, 100.0, dtype=float)
+    self.activationEnergies = []
+
+  def _getActivationEnergies(self):
+    with open(self.dataFile, 'r') as f:
+      for line in f:
+        if "SPS" in line:
+          # Parse the activation energy from the line
+          energy = line.split()[-1]
+          self.activationEnergies.append(float(energy))
+    f.close()
+
+    # Get the average activation energy
+    self._getAverageActivationEnergy()
+
+  def _getAverageActivationEnergy(self):
+    totalFrequency = 0
+    sumEnergyFreq = 0
+    for energy in self.activationEnergies:
+      if energy < 10:
+        freq = math.exp(-energy/(BOLTZMANN_CONSTANT*self.temperature))
+        totalFrequency += freq
+        sumEnergyFreq += freq*energy
+    self.averageActivationEnergy = sumEnergyFreq/totalFrequency
+
+  def _makeHistogram(self):
+    mpl.style.use('default')
+
+    histoFig, histoAxis = plt.subplots(nrows=1, ncols=1)
+    histoFig.set_size_inches(10,8)
+    histoAxis.hist(self.activationEnergies, bins="auto", color='grey', alpha=0.7, edgecolor='black', linewidth=1.0, label='Activation Energy')
+    histoAxis.axvline(x=self.averageActivationEnergy, color='blue', linestyle='--', label='Average Activation Energy')
+    histoAxis.text(self.averageActivationEnergy+0.05, -0.25, f'{self.averageActivationEnergy:.4f}', va='bottom', ha='right', color='blue')
+    histoAxis.set_xlabel('Activation Energy (eV)', fontsize=14)
+    histoAxis.set_ylabel('Count', fontsize=14)
+    histoAxis.set_title(f'Activation Energy Histogram at {self.temperature} K', fontsize=20)
+    histoAxis.yaxis.set_major_locator(plt.MaxNLocator(integer=True))
+
+    histoFig.savefig(f'{self.fileDir}/histogram.png', dpi=400, transparent=False)
+
+  def saveHistogram(self):
+    # First parse the data file for activation energies
+    self._getActivationEnergies()
+    # Create the histogram of the activation energies
+    self._makeHistogram()
---
+++ plotting.py
@@ -0,0 +1,18 @@
+# This script is used to plot the results of the dimer searces for 
+# a defect cluster or point defect. The Analysis class takes in the
+# data file from OPLD and creates a histogram of the transition states.
+
+import argparse
+
+from analysis import Analysis
+
+# Parse the command line for the data file name
+parser = argparse.ArgumentParser()
+parser.add_argument("data_file", type=str, help="data file from OPLD")
+parser.add_argument("temperature", default=300.0, type=float, help="temperature in K")
+args = parser.parse_args()
+data_file = args.data_file
+temperature = args.temperature
+
+plotter = Analysis(data_file, temperature)
+plotter.saveHistogram()
